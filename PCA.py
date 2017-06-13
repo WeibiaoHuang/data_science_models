2d67f9615ccba79f9a0a6e5f60ea1caf2d2191c6
Wilson Huang <Wilson.Huang@dentsuaegis.com>
2017-06-13 20:23:30 +0800

    second commit

--- PCA.py
+++ PCA.py
@@ -1,30 +1,8 @@
-import os
-os.chdir('C:/Dentsu/Code 1 Infinity/Infinity Reboot/codes/test')
-
-#https://www.analyticsvidhya.com/blog/2015/09/naive-bayes-explained/
-#Import Library of Gaussian Naive Bayes model
-from sklearn.naive_bayes import GaussianNB
-import numpy as np
-
-#assigning predictor and target variables
-x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
-y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
-
-#Create a Gaussian Classifier
-model = GaussianNB()
-
-# Train the model using the training sets 
-model.fit(x, y)
-
-#Predict Output 
-predicted= model.predict([[1,2],[3,4]])
-print predicted
-
-predicted2= model.predict([[3,4]])
-print predicted2
-
 #PCA
 #https://www.analyticsvidhya.com/blog/2016/03/practical-guide-principal-component-analysis-python/
+
+import os
+os.chdir('C:/Dentsu/Code 1 Infinity/Infinity Reboot/codes/test')
 
 import numpy as np
 from sklearn.decomposition import PCA
@@ -83,5 +61,3 @@ pca.fit(X)
 X1=pca.fit_transform(X)
 
 print X1
-
-#Linear Discriminant Analysis
